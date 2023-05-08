ad6852201aabaf1dc08f79e410a7fc5fa1e0e6b7
vicenteJavierveloso <vicentejavier.veloso@alumnos.ulagos.cl>
2023-05-08 11:04:35 -0400

    Añadida clase persona

---
+++ Persona.py
@@ -0,0 +1,14 @@
+class Persona:
+    def __init__(self, nombre, cintura, altura):
+        self.__nombre = nombre
+        self.__cintura = int(cintura)
+        self.__altura = int(altura)
+    
+    def getNombre(self):
+        return self.__nombre
+    def getCintura(self):
+        return self.__cintura
+    def getAltura(self):
+        return self.__altura
+    def getICA(self):
+        return self.__cintura/self.__altura
